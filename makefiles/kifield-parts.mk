bfddbb775d09fcb3150f23c95f8559a00e0cfd2e
yagamy4680 <yagamy@gmail.com>
2020-12-30 20:44:04 +0800

    add GENERATE_CHECKING_SHEETS option to kifield-parts.mk

--- makefiles/kifield-parts.mk
+++ makefiles/kifield-parts.mk
@@ -21,8 +21,13 @@ endif
 
 BOM_FOR_OCTOPART_SHEET := $(PART_DIR)/_octopart.tsv
 BOM_FOR_OCTOPART_PROCUREMENT_SHEET := $(PART_DIR)/_octopart_procurement.tsv
+BOM_FOR_CHECK1 := $(PART_DIR)/_check1.tsv
 
 ALL_SHEETS := $(BOM_GROUPED_SHEET) $(BOM_CONSOLIDATED_SHEET) $(BOM_FOR_OCTOPART_SHEET) $(BOM_FOR_OCTOPART_PROCUREMENT_SHEET)
+
+ifeq ($(GENERATE_CHECKING_SHEETS),true)
+ALL_SHEETS += $(BOM_FOR_CHECK1)
+endif
 
 all: $(ALL_SHEETS)
 
@@ -68,4 +73,16 @@ $(BOM_FOR_OCTOPART_PROCUREMENT_SHEET): $(BOM_CONSOLIDATED_SHEET)
 	cat $< | q -t -H -O "SELECT qty AS Qty, mpn AS MPN, mfr AS Manufacturer, refs AS 'Schematic Reference', value AS 'Internal Part Number', designator || ' (' || mfr || ')' AS 'Description' FROM - WHERE furnished_by = 'T2T' GROUP BY designator, mpn ORDER BY designator, mpn" > $@
 	echo "processing $(notdir $<) to generate $(notdir $@).csv ..."
 	cat $@ | q -t -H -O --output-delimiter=, 'SELECT * FROM - ' > $@.csv
+	echo ""
+
+$(BOM_FOR_CHECK1): $(BOM_SHEET)
+	echo "processing $(notdir $<) to generate $(notdir $@) ..."
+	cat $< \
+		| q -t -H -O 'SELECT * FROM - WHERE furnished_by IN ("T2T", "EMS")' \
+		| q -t -H -O "SELECT board, GROUP_CONCAT(refs, ',') AS refs, COUNT(*) AS qty, value, footprint, mfr, mpn, furnished_by, datasheet$(EXTRA_FIELDS) FROM - GROUP BY board, value, footprint, mfr, mpn, furnished_by, datasheet ORDER BY board, refs, value, footprint, mfr, mpn, furnished_by, datasheet$(EXTRA_FIELDS)" \
+		| q -t -H -O 'SELECT board || ":" || refs AS refs, qty, value, mfr, mpn, datasheet, footprint, furnished_by$(EXTRA_FIELDS) FROM -' \
+		| q -t -H -O 'SELECT GROUP_CONCAT(refs, " ") AS refs, SUM(qty) AS qty, value, footprint, mfr, mpn, furnished_by, datasheet$(EXTRA_FIELDS) FROM - GROUP BY value, footprint, mfr, mpn, furnished_by, datasheet$(EXTRA_FIELDS) ORDER BY value, footprint, mfr, mpn, furnished_by, datasheet$(EXTRA_FIELDS)' \
+		> $@
+	cat $@ | q -t -H -O --output-delimiter=, 'SELECT * FROM - ' > $@.csv
+	#cat $@ | csvlook -t | awk '{printf "\t%s\n", $$0}'
 	echo ""
