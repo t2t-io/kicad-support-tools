3277882fa5709bfdcc082450693e4722ae0091c5
yagamy4680 <yagamy@gmail.com>
2023-11-17 20:04:04 +0800

    update README

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # kicad-support-tools
 Helper scripts / tools for Kicad
+
+
+## Build Docker
+
+Build latest docker image ...
+
+```bash
+$ ./scripts/build_docker.sh
+```
+
+Clean all previous images and then build again:
+
+```bash
+$ CLEANUP=true ./scripts/build_docker.sh
+```
--- scripts/build_docker.sh
+++ scripts/build_docker.sh
@@ -6,6 +6,8 @@ cd $(dirname $0)/..
 NAME=$(basename $(pwd))
 VERSION=$(cat package.json | jq -r .version)
 
+[ "true" == "${CLEANUP}" ] && docker images | grep ${NAME} | awk '{print $2}' | sort | xargs -I{} sh -c "docker rmi ${NAME}:{}"
+
 echo "building ${NAME}:${VERSION} ..."
 docker build ${@} -t ${NAME}:${VERSION} .
 docker build ${@} -t ${NAME}:latest .
